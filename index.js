a8c3d977c8e14ef024268110d73f89dca2885c41
amalsabu59 <amalsabu59@gmail.com>
2023-08-01 23:54:16 +0530

    cart routes updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 const productsRoutes = require("./routes/products");
+const cartRoutes = require("./routes/cart");
 dotenv.config();
 app.use(express.json());
 app.use(cors());
@@ -16,6 +17,7 @@ mongoose
 
 app.use("/auth", authRoutes);
 app.use("/products", productsRoutes);
+app.use("/cart", cartRoutes);
 
 app.listen(process.env.PORT || 8800, () => {
   console.log("backend server is running !");
