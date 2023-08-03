1010527e7de132f1a2460ce84aeb9e073736d219
amalsabu59 <amalsabu59@gmail.com>
2023-08-03 23:37:11 +0530

    razorpay payment endpoints added

--- index.js
+++ index.js
@@ -7,20 +7,31 @@ const authRoutes = require("./routes/auth");
 const productsRoutes = require("./routes/products");
 const cartRoutes = require("./routes/cart");
 const addressRoutes = require("./routes/address");
+const razorpayRoutes = require("./routes/razorpay");
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("DB Connected Succesfully !"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("DB Connected Succesfully !"))
+//   .catch((err) => console.log(err));
 
 app.use("/auth", authRoutes);
 app.use("/products", productsRoutes);
 app.use("/cart", cartRoutes);
 app.use("/address", addressRoutes);
+app.use("/payment", razorpayRoutes);
 
-app.listen(process.env.PORT || 8800, () => {
-  console.log("backend server is running !");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB Connected Successfully !");
+    app.listen(process.env.PORT || 8800, () => {
+      console.log("backend server is running !");
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1); // Exit the application with an error code
+  });
